6e3f4c3bb719f32cbfbe6a6781c96ff012b18314
jkeele <jkeele@bsd220.org>
2026-02-04 10:57:17 -0600

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Jensen Keele on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
