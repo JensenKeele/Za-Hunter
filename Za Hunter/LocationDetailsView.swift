3770fdac8e877aef2c8c3153dd78b6f4f5efd409
jkeele <jkeele@bsd220.org>
2026-02-22 09:13:14 -0600

    added details about pizza places

---
+++ Za Hunter/LocationDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailsView.swift
+//  Za Hunter
+//
+//  Created by Jensen Keele on 2/22/26.
+//
+
+import SwiftUI
+
+struct LocationDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailsView()
+}
